89f69a544db2c3d24281593dc617e4b49ae34869
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-08-02 23:02:51 +0530

    1

--- src/main/java/com/pn/javafx_application/HelloApplication.java
+++ src/main/java/com/pn/javafx_application/HelloApplication.java
@@ -11,10 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 275);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/pn/javafx_application/HelloController.java
+++ src/main/java/com/pn/javafx_application/HelloController.java
@@ -1,14 +1,28 @@
 package com.pn.javafx_application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Label label;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button4;
+    public void initialize(){
+        button4.setEffect(new DropShadow());
     }
+    @FXML
+    public void handelMouseEnter(){
+        label.setScaleY(2.0);
+        label.setScaleX(2.0);
+    }
+    @FXML
+    public void handelMouseExit(){
+        label.setScaleX(1.0);
+        label.setScaleX(1.0);
+    }
+
+
 }
