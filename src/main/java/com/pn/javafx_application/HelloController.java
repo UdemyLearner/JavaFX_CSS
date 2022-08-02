637538db75fa7d113cb055588f657c9b297d0fd5
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-08-02 23:19:37 +0530

    File Chooser

--- src/main/java/com/pn/javafx_application/HelloController.java
+++ src/main/java/com/pn/javafx_application/HelloController.java
@@ -4,12 +4,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
     @FXML
     private Label label;
     @FXML
     private Button button4;
+    @FXML
+    private GridPane gridPane;
+
     public void initialize(){
         button4.setEffect(new DropShadow());
     }
@@ -24,5 +32,16 @@ public class HelloController {
         label.setScaleX(1.0);
     }
 
+    @FXML
+    public void handelClick() {
+        //FileChooser chooser = new FileChooser();
+        DirectoryChooser chooser = new DirectoryChooser();
+        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        if (file != null) {
+            System.out.println(file.getPath());
+        }else {
+            System.out.println("Chooser was cancelled ");
+        }
 
+    }
 }
